8aa4012c3bb30caa019fce06fb99380d9d380780
BBOSS <andrea.diiorio.sniper@gmail.com>
2018-05-24 11:27:13 +0200

    commit iniziale

---
+++ app/src/main/java/com/example/bboss/btrockscissorpaper/MainActivity.java
@@ -0,0 +1,241 @@
+package com.example.bboss.btrockscissorpaper;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+/*
+TODO CANCELLA : TUTTE DIR BUILD...
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textViewInfos;
+    private Button btnServer;
+    private Button btnClient;
+    private String hostKind;
+    private ListView listViewDiscovered;
+    public static final String CLIENT="CLIENT";
+    private List<BluetoothDevice> discovered = new ArrayList<>();
+    public static final String SERVER="SERVER";
+    private static final int REQUEST_ENABLE_BT=1;   //passed with intent to bt os handler...retrived in onREsult...
+    private BluetoothAdapter bluetoothAdapter;
+
+    private final int DURATION=300;
+    private final UUID uuid= UUID.fromString("b8319a04-3632-4d0d-8bd5-47238a404a28");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnClient = findViewById(R.id.btnClient);
+        btnServer = findViewById(R.id.btnServer);
+        listViewDiscovered = findViewById(R.id.discovered);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnServer.setOnClickListener(this);
+        btnClient.setOnClickListener(this);
+
+
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnClient:
+                hostKind=CLIENT;
+                break;
+            case R.id.btnServer:
+                hostKind=SERVER;
+                break;
+            default:
+                throw new IllegalArgumentException("CLICK ONLY SERVER OR CLIENT BTN... :(");
+
+            }
+        this.connectTry();
+    }
+    private void connectTry(){
+        System.out.println("SELECTED..."+hostKind);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter==null){
+            BTHandler.setupAllert("BT NOT SUPPORTED FROM DEVICE");
+            finish();
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
+        }
+
+        if (hostKind.equals(SERVER))
+            connectTryServer();
+        else
+            connectTryClient();
+    }
+
+    private void connectTryServer() {
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DURATION);
+        startActivity(discoverableIntent);
+        //getting server socket
+        /*
+        BluetoothServerSocket bluetoothServerSocket= null;
+        try {
+            bluetoothServerSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(this.getLocalClassName(),uuid);
+            //SDP protocol where run bt? has localClassName and uuid specified
+            BluetoothSocket serverSocket= bluetoothServerSocket.accept(); //blocking call... MUST BE IN ANTOHER TH!
+
+        } catch (IOException e) {
+            BTHandler.setupAllert("ERROR IN CREATE COMUNICATION CHANNEL (SERVER)");
+            e.printStackTrace();
+        }
+        try {
+            bluetoothServerSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //getted connection==>close way to get more ... //TODO CHANGE FOR >=3 PLAYER!
+        */
+    }
+    //BROADCAST RECEIVER 4 CLIENT
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                discovered.add(device);
+                //discovered.add(device);
+                //TODO ADD TO A LIST OF FOUNDED DEVICE (HAS TO BE CLEARED BEFORE START SCANNING!
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                System.out.println(deviceName+deviceHardwareAddress);
+            }
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+                //TODO HANLDE ?
+                ArrayAdapter<BluetoothDevice> arrayAdapter= new ArrayAdapter<BluetoothDevice>(context,R.id.textView,discovered);
+                listViewDiscovered.setAdapter(arrayAdapter);
+
+            }
+
+        }
+    };
+    private void connectTryClient(){
+        //TODO THIS METHOD HAS TO WRAP CALLING TO ANOTHER CLASS WITCH WILL PERFORM ALL IN ANOTHER THREAD
+        BluetoothDevice targetDevice=null;
+        //getting paierd devices...
+        System.out.println("PAIRED DEVICES...");
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        discovered.addAll(pairedDevices);   //TODO DEBUG ONLY
+        /*TODO TEST
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                System.out.println("DEVICE"+deviceName+deviceHardwareAddress);
+            }
+        }
+        */
+
+        //DISCOVERING OTHER DEVICE...
+        // Create a BroadcastReceiver for ACTION_FOUND.
+
+
+        IntentFilter filterFound = new IntentFilter(BluetoothDevice.ACTION_FOUND);  //TODO NERVER FOUND NOTHING ... :(
+        IntentFilter filterDiscChanged = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filterFound);
+        registerReceiver(mReceiver, filterDiscChanged);
+
+        //getting runtime permission to access course location
+        int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        listViewDiscovered.setVisibility(View.VISIBLE);
+
+        boolean returnB= bluetoothAdapter.startDiscovery();   //manualy handle bt discvoery in code!!
+        ArrayAdapter<BluetoothDevice> arrayAdapter= new ArrayAdapter<BluetoothDevice>(this,R.id.discovered,discovered);
+        listViewDiscovered.setAdapter(arrayAdapter);
+        Intent intentBluetooth = new Intent();
+        intentBluetooth.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+        //startActivityForResult(intentBluetooth,11);//TODO CHECK START SYS DISCOVERY ACTIVITY!
+        //unbinding receiver of broadcast bt discovery notify... TODO EVALUTATE DIFFERENT POSITION (LK ONDESTROY)
+        //unregisterReceiver(mReceiver); //TODO ADD
+        BluetoothSocket clientSocket = null;
+        /*
+        try {
+
+            clientSocket= targetDevice.createInsecureRfcommSocketToServiceRecord(uuid);
+            clientSocket.connect(); //TODO BLOCKING UNTIL CLIENT AND SERVER HAVE PAIRED...
+            //IOEXEPTION FOR TIMEOUT---ERRORS
+        } catch (IOException e) {
+            BTHandler.setupAllert("ERROR IN CREATE COMUNICATION CHANNEL CLIENT");
+            e.printStackTrace();
+        }
+        */
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //todo cases with result canceled... exit or retry?(other way to get this..??)
+        switch (requestCode){
+            case REQUEST_ENABLE_BT: //TODO RELATION.. SAME CODE SENDED TO START ACTIVITY RETRIVED HERE
+                //result from enabling BT..:D
+                if (resultCode== Activity.RESULT_OK){
+                    System.out.println("BT ENABLED SUSSFULY");
+                }
+                else if(resultCode==Activity.RESULT_CANCELED){      //pressed no on enabling bt..
+                    BTHandler.setupAllert("ERROR IN ENABLING...:'((");
+                }
+                break;
+            case (DURATION):{ //DOCS SAY RESULT OF BT DISCOVERABLE SWITCH USE THIS RET CODE...
+                //server calling
+                    if(resultCode==RESULT_CANCELED){
+                        BTHandler.setupAllert("ERROR IN DISCOVERABILITY");
+                    }
+                    else if (resultCode==RESULT_OK){
+                        System.out.println("OK DISCOVERABILITY SWITCH");
+                    }
+            }
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+}
